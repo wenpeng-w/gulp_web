f29eb744d8fbeba7a3c2e697d7944323d150effa
wenpeng-w <449202631@qq.com>
2017-11-20 22:22:37 +0800

    add panel

---
+++ dist/js/base.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 2017/11/19.
+ */
+(function () {
+    console.log('一份耕耘，一分收获。');
+    console.log('http://womenl.cn');
+    console.log('                  @Yiran');
+})()
