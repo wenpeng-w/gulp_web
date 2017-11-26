a89c73f6132ba99b82a1e87171b3d9a048fa60fc
wenpeng-w <449202631@qq.com>
2017-11-26 15:43:28 +0800

    修改配置文件和目录结构

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@
 
 // 创建本地服务器 gulp-connect
 // 合并文件 gulp-concat
-// 最小化 css 文件 gulp-minify-css
+// 最小化 css 文件 gulp-clean-css ( gulp-minify-css 已废弃）
 // 最小化 js 文件 gulp-minify
 // 压缩html 文件 gulp-minify-html
 // 重命名文件 gulp-rename
@@ -14,11 +14,12 @@
 
 
 // 载入gulp核心包
-var gulp = require('gulp');
-var less = require('gulp-less');
-var prettify = require('gulp-jsbeautifier');
-var ejs = require('gulp-ejs');
-var browserSync = require('browser-sync').create();
+var gulp = require('gulp'),
+    less = require('gulp-less'),
+    cssMinify = require('gulp-clean-css'),
+    prettify = require('gulp-jsbeautifier'),
+    ejs = require('gulp-ejs'),
+    browserSync = require('browser-sync').create();
 
 // 定义任务 第一个参数是任务名，第二个参数是任务执行体
 
@@ -36,13 +37,34 @@ gulp.task('html', function () {
 // 拷贝css的任务
 gulp.task('copycss', function () {
     gulp.src('src/**/*.css')
+        .pipe(cssMinify({
+            advanced: false, // 类型：Boolean 默认：true [是否开启高级优化（合并选择器等）]
+            compatibility: 'ie8',
+            format: 'keep-breaks', // 类型：Boolean 默认：false [是否保留换行]
+            keepSpecialComments: '*' // 保留所有特殊前缀 当你用autoprefixer生成的浏览器前缀，如果不加这个参数，有可能将会删除你的部分前缀
+        }))
         .pipe(gulp.dest('dist/'))
         .pipe(browserSync.reload({
             stream: true
         }));
 });
 
-// 拷贝css的任务
+// 新建 less 的任务
+gulp.task('less', function () {
+    gulp.src('src/**/*.less')
+        .pipe(less())
+        .pipe(cssMinify({
+            advanced: false, // 类型：Boolean 默认：true [是否开启高级优化（合并选择器等）]
+            compatibility: 'ie8',
+            format: 'keep-breaks', // 类型：Boolean 默认：false [是否保留换行]
+            keepSpecialComments: '*' // 保留所有特殊前缀 当你用autoprefixer生成的浏览器前缀，如果不加这个参数，有可能将会删除你的部分前缀
+        }))
+        .pipe(gulp.dest('dist/'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});
+// 拷贝js的任务
 gulp.task('copyjs', function () {
     gulp.src('src/**/*.js')
         .pipe(gulp.dest('dist/'))
@@ -55,16 +77,6 @@ gulp.task('copyjs', function () {
 gulp.task('copyimage', function () {
     gulp.src('src/image/*')
         .pipe(gulp.dest('dist/image/'))
-        .pipe(browserSync.reload({
-            stream: true
-        }));
-});
-
-// 新建css的任务
-gulp.task('less', function () {
-    gulp.src('src/**/*.less')
-        .pipe(less())  // 编译less文件
-        .pipe(gulp.dest('dist/'))
         .pipe(browserSync.reload({
             stream: true
         }));
